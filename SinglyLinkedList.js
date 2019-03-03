ef06ec78410e59f5f004ef448f6ed85ef6936a14
Abdul Ghaffar <aghaffar570@gmail.com>
2019-03-03 10:19:16 -0500

    add prepend and removeHead method for SLL

--- SinglyLinkedList.js
+++ SinglyLinkedList.js
@@ -11,4 +11,35 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  prepend(item) {
+    const newNode = new Node(item);
+
+    if(!this.tail) {
+      this.head = newNode;
+      this.tail = this.head;
+      return this;
+    }
+
+    newNode.next = this.head;
+    this.head = newNode;
+
+    this.length++;
+    return this;
+  }
+
+  removeHead() {
+
+    if(!this.head) return undefined;
+    this.length--;
+
+    const removedHead = this.head;
+    this.head = removedHead.next;
+    removedHead.next = null;
+
+    if(!this.length) this.tail = null;
+
+    return removedHead;
+  }
+
 }
