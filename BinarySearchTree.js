721cba4be401a2c5f3973eefe72a1188eabcdcdb
Abdul Ghaffar <aghaffar570@gmail.com>
2019-03-06 15:16:40 -0500

    add insert and contains method to BST

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -10,4 +10,19 @@ class BinarySearchTree {
     this.length = 0;
   }
 
+  insert(val) {
+    const direction = val < this.value ? 'left' : 'right';
+    if(this[direction]) this[direction].insert(val);
+    else this[direction] = new BinarySearchTree(val);
+    this.length++
+  }
+
+  contains(val) {
+    if(val === this.value) return true;
+
+    const direction = val < this.value ? 'left' : 'right';
+    if(this[direction]) return this[direction].contains(val);
+    else return false;
+  }
+
 }
