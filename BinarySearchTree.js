d6506b6fa1f532a9b417a3b77974724f0c62dd98
Abdul Ghaffar <aghaffar570@gmail.com>
2019-03-06 15:13:41 -0500

    add BST

---
+++ BinarySearchTree.js
@@ -0,0 +1,13 @@
+// O(log n) insertion
+// O(log n) searching
+
+
+class BinarySearchTree {
+  constructor(val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+    this.length = 0;
+  }
+
+}
