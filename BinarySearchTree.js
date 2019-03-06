22ffd029936ce60ade61bf257331016c6ed06d96
Abdul Ghaffar <aghaffar570@gmail.com>
2019-03-06 15:24:21 -0500

    add traversing methods BST

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -12,8 +12,10 @@ class BinarySearchTree {
 
   insert(val) {
     const direction = val < this.value ? 'left' : 'right';
+
     if(this[direction]) this[direction].insert(val);
     else this[direction] = new BinarySearchTree(val);
+
     this.length++
   }
 
@@ -21,8 +23,32 @@ class BinarySearchTree {
     if(val === this.value) return true;
 
     const direction = val < this.value ? 'left' : 'right';
+
     if(this[direction]) return this[direction].contains(val);
     else return false;
+  }
+
+  depthFirst(callback, option = 'in-order') {
+    if(option === 'pre-order') callback(this.value);
+
+    if(this.left) this.depthFirst(callback, option)
+
+    if(option === 'in-order') callback(this.value);
+
+    if(this.right) this.depthFirst(callback, option)
+
+    if(option === 'post-order') callback(this.value);
+  }
+
+  breadthFirst(callback) {
+    const queue = [this];
+
+    while (queue.length) {
+      const current = queue.shift();
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+      callback(current.value);
+    }
   }
 
 }
